8d0fd208911cd75741b8ac82c3e0310514f9cdcf
German <german220291@gmail.com>
2020-03-06 14:16:52 +0000

    Added storyboard protocol

--- RickMortyUniverse/Protocols/Storyboarded.swift
+++ RickMortyUniverse/Protocols/Storyboarded.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2020 German Hernandez. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+protocol Storyboarded: ModeledViewController {
+    static func instantiateFromStoryboard(with viewModel: ViewModel) -> Self
+}
+
+extension Storyboarded where Self: UIViewController {
+    
+    static func instantiateFromStoryboard(with viewModel: ViewModel) -> Self {
+        let className = NSStringFromClass(self).components(separatedBy: ".")[1]
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        let vc = storyboard.instantiateViewController(withIdentifier: className) as! Self
+        vc.viewModel = (viewModel as! Self.AnyViewModel)
+        vc.setAsDelegate(for: viewModel)
+        return vc
+    }
+}
