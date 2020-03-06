a8e5892fd3ac746ee7cb18798869978f41801943
German <german220291@gmail.com>
2020-03-06 15:02:39 +0000

    Refactored and cleaned up

--- RickMortyUniverse/Coordinators/AppCoordinator.swift
+++ RickMortyUniverse/Coordinators/AppCoordinator.swift
@@ -14,26 +14,9 @@ class AppCoordinator: Coordinator {
     var childCoordinator: Coordinator?
     var rootViewController: UIViewController
     
-    let tabBarController = UITabBarController()
-    let charactersCoordinator = CharactersCoordinator()
-    let locationsCoordinator = LocationsCoordinator()
-    let episodesCoordinator = EpisodesCoordinator()
+    let tabBarController = RMNavigationController()
     
     required init() {
-        let characterItem = UITabBarItem(title: "Characters", image: UIImage(systemName: "person.3.fill"), tag: 1)
-        let locationItem = UITabBarItem(title: "Locations", image: UIImage(systemName: "location.fill"), tag: 2)
-        let episodeItem = UITabBarItem(title: "Episodes", image: UIImage(systemName: "book.fill"), tag: 3)
-        
-        charactersCoordinator.rootViewController.tabBarItem = characterItem
-        locationsCoordinator.rootViewController.tabBarItem = locationItem
-        episodesCoordinator.rootViewController.tabBarItem = episodeItem
-        
-        tabBarController.viewControllers = [
-            charactersCoordinator.rootViewController,
-            locationsCoordinator.rootViewController,
-            episodesCoordinator.rootViewController,
-        ]
-        
         rootViewController = tabBarController
     }
 }
---
+++ RickMortyUniverse/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  RickMortyUniverse
+//
+//  Created by German Hernandez on 06/03/2020.
+//  Copyright © 2020 German Hernandez. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    
+    struct SFSymbols {
+        static let people = UIImage(systemName: "person.3.fill")
+        static let location = UIImage(systemName: "location.fill")
+        static let book = UIImage(systemName: "book.fill")
+    }
+}
