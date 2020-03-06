57631795359c50450dd994a661a2ab9c905c2f71
German <german220291@gmail.com>
2020-03-06 13:58:47 +0000

    Added MVVV-C required files

---
+++ RickMortyUniverse/Coordinators/EpisodeDetailCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  EpisodeDetailCoordinator.swift
+//  RickMortyUniverse
+//
+//  Created by German Hernandez on 06/03/2020.
+//  Copyright © 2020 German Hernandez. All rights reserved.
+//
+
+import Foundation
